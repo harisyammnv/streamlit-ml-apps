f21ca2339ab8c67f7d75d3fb8b026e7d2ba85d13
harisyammnv <harisyam.bphc@gmail.com>
2020-12-15 10:51:53 +0100

    simple lstm

--- pytorch_practice/simle_rnn.py
+++ pytorch_practice/simle_rnn.py
@@ -14,12 +14,13 @@ class RNN(nn.Module):
         super(RNN, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
-        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
+        self.LSTM = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size * NetworkParameters.sequence_length, num_classes)
 
     def forward(self, x):
         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
-        out, _ = self.rnn(x, h0)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+        out, _ = self.lstm(x, (h0, c0))
         out = out.reshape(out.shape[0], -1)
         out = self.fc(out)
         return out
